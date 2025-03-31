773cfea3760cf83649cc85710cd74cd84734a426
unknown <latakemayur@gmail.com>
2025-03-31 19:21:19 +0530

    added plugin name

---
+++ custom-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Custom Plugin
+ * Plugin URI:  https://mayurlatake.tech
+ * Description: A simple WordPress plugin template.
+ * Version:     1.0
+ * Author:      Mayur Latake
+ * Author URI:  https://mayurlatake.tech
+ * License:     GPL2
+ */
